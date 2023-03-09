3d5dbe186382db79b5b2fde128e35f88f75f2f41
Md. Ripon Miah <88526572+ripon2488@users.noreply.github.com>
2023-03-10 02:58:31 +0600

    Update heart_disease_app.py

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -4,7 +4,7 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import pickle
 import sklearn
 
-st.image('header.jpg')
+st.image('Header.jpg')
 st.title(':blue[Heart disease Prediction App]')
 st.write("""-- This app predicts A patient has a heart disease or not --
 
