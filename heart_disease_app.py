48113f6d65e8380ba73cfd6dcc3dc7f77cb06330
Md. Ripon Miah <88526572+ripon2488@users.noreply.github.com>
2023-03-10 03:41:26 +0600

    Update heart_disease_app.py

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -57,9 +57,9 @@ if st.button("Click here to Predict type of Disease"):
         
 #https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
 st.write("""
-<h1> Dream towards Data Scientist </h1><br>
 
-<h2>🤠 About Me: </h2><br>
+
+# 🤠 About Me: 
 I always enjoyed Computer Science, Data Science and machine learning that has drove me to be an Engineer. <br>
 
 
@@ -73,9 +73,10 @@ I always enjoyed Computer Science, Data Science and machine learning that has dr
 <ul>- 😄 Pronouns: Ripon (রিপন)</ul>
 <ul>- ⚡ Fun fact: </ul>
 
+
 <br>
-<h3> More Details for me:  https://sites.google.com/view/ripon2488</h3>
-<h2> ✨ Tech Knowledge: </h2><br>
+# More Details for me:  https://sites.google.com/view/ripon2488
+# ✨ Tech Knowledge: 
 
 ![](https://img.shields.io/badge/python-3670A0?style=for-the-badge&amp;logo=python&amp;logoColor=ffdd54)
 ![](https://img.shields.io/badge/Keras-%23D00000.svg?style=for-the-badge&amp;logo=Keras&amp;logoColor=white)
@@ -93,21 +94,6 @@ I always enjoyed Computer Science, Data Science and machine learning that has dr
 ![](https://www.googleapis.com/download/storage/v1/b/kaggle-user-content/o/inbox%2F9733051%2F224ba3f0a7a6dd52c6c5d57b4c6768bc%2Fmysql.JPG?generation=1674674585250106&alt=media)
 ![](https://www.googleapis.com/download/storage/v1/b/kaggle-user-content/o/inbox%2F9733051%2F451decb991b663ac2a01bf33287e8f89%2Foracle.JPG?generation=1674674585317290&alt=media)
 
-
-<br>
-
-<p align="center">
-
-  <br/>
-  <p align="center">🥇My Kaggle Tier & Medals 🥇</p>
- 
-</p>
-<p align="center">
-  <img src="https://road-to-kaggle-grandmaster.vercel.app/api/badges/mdriponmiah/competition/"; />
-  <img src="https://road-to-kaggle-grandmaster.vercel.app/api/badges/mdriponmiah/dataset/"; />
-  <img src="https://road-to-kaggle-grandmaster.vercel.app/api/badges/mdriponmiah/notebook/"; />
-  <img src="https://road-to-kaggle-grandmaster.vercel.app/api/badges/mdriponmiah/discussion/"; />
-</p>
 
 ![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fmdriponmiah%2Fkaggle-badge&count_bg=%23DDAA17&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)
 ![mdriponmiah](https://road-to-kaggle-grandmaster.vercel.app/api/simple/mdriponmiah) 
