88fb56bed30b80d8ddf65f86785b86961cecebe3
Md. Ripon Miah <88526572+ripon2488@users.noreply.github.com>
2023-03-10 03:37:21 +0600

    Update heart_disease_app.py

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -10,7 +10,7 @@ st.title(':blue[Heart disease Prediction App]')
 st.write("""-- This app predicts A patient has a heart disease or not --
 
 """)
-st.download_button('Download Sample file for check', 'https://github.com/ripon2488/heart-disease-prediction-machine-learning/blob/main/heart_disease_dataset.csv')
+st.download_button('Download Sample file link for check', 'https://github.com/ripon2488/heart-disease-prediction-machine-learning/blob/main/heart_disease_dataset.csv')
 st.sidebar.header('Please Input Features Value')
 
 # Collects user input features into dataframe
@@ -57,3 +57,59 @@ if st.button("Click here to Predict type of Disease"):
         
 #https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
 
+<h1> Dream towards Data Scientist </h1><br>
+
+<h2>🤠 About Me: </h2><br>
+I always enjoyed Computer Science, Data Science and machine learning that has drove me to be an Engineer. <br>
+
+
+<ul>- 🔭 I’m currently working on Database Administration, ML and DL.</ul>
+<ul>- 🌱 I’m currently learning Data Analyics, Machine Learning ,Deep Learning and Data Science.</ul>
+<ul>- 👯 I’m looking to collaborate on Data Engineering and AI.</ul>
+<ul>- 🤔 I’m looking for help with Data Science and AI.</ul>
+<ul>- 💬 Ask me about Data Analysis, Engineering and ML.</ul>
+<ul>- 📫 How to reach me: <a href="https://www.linkedin.com/in/ripon2488/"> ripon2488 </a> </ul>
+<ul>- 📫 Workflow in Kaggle: <a href="https://www.kaggle.com/mdriponmiah"> mdriponmiah </a> </ul>
+<ul>- 😄 Pronouns: Ripon (রিপন)</ul>
+<ul>- ⚡ Fun fact: </ul>
+
+<br>
+<h3> More Details for me:  https://sites.google.com/view/ripon2488</h3>
+<h2> ✨ Tech Knowledge: </h2><br>
+
+![](https://img.shields.io/badge/python-3670A0?style=for-the-badge&amp;logo=python&amp;logoColor=ffdd54)
+![](https://img.shields.io/badge/Keras-%23D00000.svg?style=for-the-badge&amp;logo=Keras&amp;logoColor=white)
+![](https://img.shields.io/badge/numpy-%23013243.svg?style=for-the-badge&amp;logo=numpy&amp;logoColor=white)
+![](https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&amp;logo=pandas&amp;logoColor=white)
+![](https://img.shields.io/badge/Plotly-%233F4F75.svg?style=for-the-badge&amp;logo=plotly&amp;logoColor=white)
+ ![](https://img.shields.io/badge/PyTorch-%23EE4C2C.svg?style=for-the-badge&amp;logo=PyTorch&amp;logoColor=white)
+![](https://img.shields.io/badge/scikit--learn-%23F7931E.svg?style=for-the-badge&amp;logo=scikit-learn&amp;logoColor=white) 
+![](https://img.shields.io/badge/SciPy-%230C55A5.svg?style=for-the-badge&amp;logo=scipy&amp;logoColor=%white)
+![](https://img.shields.io/badge/TensorFlow-%23FF6F00.svg?style=for-the-badge&amp;logo=TensorFlow&amp;logoColor=white)
+![](https://img.shields.io/badge/opencv-%23white.svg?style=for-the-badge&amp;logo=opencv&amp;logoColor=white)
+![](https://www.googleapis.com/download/storage/v1/b/kaggle-user-content/o/inbox%2F9733051%2F9de19bc8674de7e909cfdc555ab8199b%2Fpower%20bi.JPG?generation=1674674584825248&alt=media)
+![](https://www.googleapis.com/download/storage/v1/b/kaggle-user-content/o/inbox%2F9733051%2F2984bf7961a04d79aa992de7e25fa036%2Ftableau.JPG?generation=1674674585096135&alt=media)
+![](https://www.googleapis.com/download/storage/v1/b/kaggle-user-content/o/inbox%2F9733051%2F9b2e7383176bb5d806142e8ef8f89bb5%2Fgoogle%20data%20studio.JPG?generation=1674674585135850&alt=media)
+![](https://www.googleapis.com/download/storage/v1/b/kaggle-user-content/o/inbox%2F9733051%2F224ba3f0a7a6dd52c6c5d57b4c6768bc%2Fmysql.JPG?generation=1674674585250106&alt=media)
+![](https://www.googleapis.com/download/storage/v1/b/kaggle-user-content/o/inbox%2F9733051%2F451decb991b663ac2a01bf33287e8f89%2Foracle.JPG?generation=1674674585317290&alt=media)
+
+
+<br>
+
+<p align="center">
+
+  <br/>
+  <p align="center">🥇My Kaggle Tier & Medals 🥇</p>
+ 
+</p>
+<p align="center">
+  <img src="https://road-to-kaggle-grandmaster.vercel.app/api/badges/mdriponmiah/competition/"; />
+  <img src="https://road-to-kaggle-grandmaster.vercel.app/api/badges/mdriponmiah/dataset/"; />
+  <img src="https://road-to-kaggle-grandmaster.vercel.app/api/badges/mdriponmiah/notebook/"; />
+  <img src="https://road-to-kaggle-grandmaster.vercel.app/api/badges/mdriponmiah/discussion/"; />
+</p>
+
+![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fmdriponmiah%2Fkaggle-badge&count_bg=%23DDAA17&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)
+![mdriponmiah](https://road-to-kaggle-grandmaster.vercel.app/api/simple/mdriponmiah) 
+
+                                                                                        
